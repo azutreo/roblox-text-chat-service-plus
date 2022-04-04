00e479201023bf8f0379f2cdc51725bc5613528f
Nicholas Foreman <nicholas.w.foreman@outlook.com>
2022-04-04 07:20:09 -0500

    Once again, same as previous commit :(

--- Source/TextChatServicePlus/Configuration/init.lua
+++ Source/TextChatServicePlus/Configuration/init.lua
@@ -15,8 +15,8 @@ Configuration.PrefixFormat = "<font color='#%s'><b>%s</b></font> %s"
 Configuration.NameColorFormat = "<font color='#%s'>%s</font>"
 Configuration.ChatColorFormat = "<font color='#%s'>%s</font>"
 
-Configuration.Prefixes = require(script.Configuration.Prefixes)
-Configuration.NameColors = require(script.Configuration.NameColors)
-Configuration.ChatColors = require(script.Configuration.ChatColors)
+Configuration.Prefixes = require(script.Prefixes)
+Configuration.NameColors = require(script.NameColors)
+Configuration.ChatColors = require(script.ChatColors)
 
 return Configuration
